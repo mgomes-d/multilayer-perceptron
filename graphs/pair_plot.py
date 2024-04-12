3dcc2d25485ebfc8a59cb5d162884967ca80c384
mgomes-d <murillo.jogos8@hotmail.com>
2024-04-12 07:58:25 +0200

    graphs/pair_plot.py remove useless comment

--- graphs/pair_plot.py
+++ graphs/pair_plot.py
@@ -4,8 +4,6 @@ import matplotlib.pyplot as plt
 def pair_plot(df):
 
     features_subset = df[df.columns[21:]].copy()
-    # features_subset.columns += 'Diagnosis'
     features_subset['Diagnosis'] = df['Diagnosis']
-    # print(features_subset)
     pair_plot = sns.pairplot(features_subset, hue='Diagnosis')
     plt.show()
