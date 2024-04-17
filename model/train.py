07c31852346a3daacbfed88d177a18871ec36fab
mgomes-d <murillo.jogos8@hotmail.com>
2024-04-17 12:05:51 +0200

    weight init " create functions to call the right activation func starting the weight initiator
    exit

--- model/train.py
+++ model/train.py
@@ -1,12 +1,33 @@
 import numpy as np
 
 class perceptron:
-    def __init__(self, nb, init_weight, activation):
+    def __init__(self, nb, weight_init:float, activation):
         self.id = nb
-        self.weight = init_weight
-        self.activation = activation
-    def hello(self):
-        print("HEllo")
+        self.weight = weight_init
+        self.activation = self.get_activation_function(activation)
+
+    def get_activation_function(self, activation):
+        activations_names = ["sigmoid", "hyperboloid tangent", "rectified linear unit"]
+        functions_pointers = [self.sigmoid, self.hyperboloid_tangent, self.rectified_linear_unit]
+
+        if activation in activations_names:
+            index = activations_names.index(activation)
+            return functions_pointers[index]
+        else:
+            raise ValueError("Activation function not supported")
+
+    def sigmoid(self, x):
+        return 1 / (1 + np.exp(-x))
+
+    def hyperboloid_tangent(self, x):
+        return np.tanh(x)
+
+    def rectified_linear_unit(self, x):
+        return np.maximum(0, x)
+    
+    def get_weight(self):
+        return self.weight
+            
 
 class layers:
     def __init__(self):
@@ -14,17 +35,38 @@ class layers:
 
     def DenseLayer(self, nb_perceptron, activation, weights_initializer):
         perceptrons = []
+        weights_init = self.init_weights(weights_initializer, nb_perceptron)
         for i in range(nb_perceptron):
-            perceptrons.append(perceptron(i, self.init_weight(), activation))
+            perceptrons.append(perceptron(i, weights_init, activation))
         self.layers.append(perceptrons)
         print(self.layers)
-        
-    def init_weight(self):
+
+    def init_weights(self, weights_initializer, nbs):
+        weights = self.get_weights_initializer_function(weights_initializer)
         return 0
 
-    def sigmoid(self, x):
-        return (1 / 1 + np.exp(-x))
+    def get_weights_initializer_function(self, weight_initializer):
+        weights_init_names = ["HeUniform", "GlorotUniform", "LeCunUniform", "RandomUniform"]
+        functions_pointers = [self.he_uniform, self.glorot_uniform, self.lecun_uniform, \
+        self.random_uniform]
 
+        if weight_initializer in weights_init_names:
+            index = weights_init_names.index(weight_initializer)
+            return functions_pointers[index]
+        else:
+            raise ValueError("Weight function not supported")
+
+    def he_uniform(self, nbs):
+        return 0
+    
+    def glorot_uniform(self, nbs):
+        return 0
+
+    def lecun_uniform(self, nbs):
+        return 0
+
+    def random_uniform(self, nbs):
+        return 0
 
 class model:
     def __init__(self):
@@ -92,13 +134,12 @@ class model:
         self.learning_rate = learning_rate
         self.batch_size = batch_size
         self.epochs = epochs
-
-
-    def fit(self, network, data_train, data_valid, loss, learning_rate, batch_size, epochs):
-        self.init_values(network, data_train, data_valid, loss, learning_rate, batch_size, epochs)
         self.make_mean_std(self.data_train)
         self.normalize_data(self.data_train)
         self.normalize_data(self.data_valid)
         self.get_predict_values(self.data_train)
         self.parse_data(self.data_train, True)
         self.parse_data(self.data_valid)
+
+    def fit(self, network, data_train, data_valid, loss, learning_rate, batch_size, epochs):
+        self.init_values(network, data_train, data_valid, loss, learning_rate, batch_size, epochs)
